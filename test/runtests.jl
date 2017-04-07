5f23765b83cfd3a87bd36826dd53bc9249d42129
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 05:38:43 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,3 @@ using Base.Test
 
 @test prev_float(sqrt(0.25)) == prevfloat(sqrt(0.25))
 @test next_float(sqrt(0.25)) == nextfloat(sqrt(0.25))
-
-@test prev_float(4096*realmin(Float32)) == prevfloat(4096*realmin(Float32))
-@test next_float(4096*realmin(Float32)) == nextfloat(4096*realmin(Float32))
-
-@test next_float(realmin(Float64)) == nextfloat(nextfloat(realmin(Float64)))
