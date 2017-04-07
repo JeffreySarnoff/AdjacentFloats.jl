3d5358b058060f2c1034bc733042ae563831885c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-06 21:57:03 -0400

    Update AdjacentFloats.jl

--- src/AdjacentFloats.jl
+++ src/AdjacentFloats.jl
@@ -4,20 +4,6 @@ export prev_float, next_float
 
 using Compat
 
-#=
-  These routines return +/-Inf when given +/-Inf.
-  That differs from nextfloat(-Inf) == -realmax(), prevfloat(Inf) == realmax()
-  (prevfloat(Inf)==Inf makes more sense to me, and likely is more helpful).
-  The logic for finite Float64 and Float32 values steps twice when 
-  given values of very small magnitude (see paper).
-  The alternative implementation, converting to [U]Int and adding/subtracting 1,
-  returns NaN when given +/-Inf -- and checking for Inf adds branching. 
-  ref:
-  'Predecessor and Successor In Rounding To Nearest' by Siegried M. Rump
-  "The routines deliver the exact answer except for a small range near underflow,
-   in which case the true result is overestimated by eta [the value added/subtracted below]."
-=#
-
 # exact for |x| > 8.900295434028806e-308, otherwise may be two steps rather than one step
 next_nearerto_zero(x::Float64)   = (0.9999999999999999*x)-5.0e-324 # (x-1.1102230246251568e-16*x)-5.0e-324 
 next_awayfrom_zero(x::Float64)   = (1.0000000000000002*x)+5.0e-324 # (x+1.1102230246251568e-16*x)+5.0e-324
