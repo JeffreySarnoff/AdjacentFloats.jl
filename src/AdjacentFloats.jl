0c1b386c74a5992ac61a545baae88479deb7174f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-17 12:42:45 -0400

    Update AdjacentFloats.jl

--- src/AdjacentFloats.jl
+++ src/AdjacentFloats.jl
@@ -10,12 +10,12 @@ prev_float{T<:IEEE754}(x::T) = signbit(x) ? next_awayfrom_zero(x) : next_nearert
 
 
 # exact for |x| > 8.900295434028806e-308, otherwise may be two steps rather than one step. |x| > ldexp(0.5, -1019)
-next_nearerto_zero(x::Float64)   = muladd(0.9999999999999999, x, -5.0e-324)  # (x-1.1102230246251568e-16*x)-5.0e-324 
-next_awayfrom_zero(x::Float64)   = muladd(1.0000000000000002, x, +5.0e-324)  # (x+1.1102230246251568e-16*x)+5.0e-324
+next_nearerto_zero(x::Float64)   = (x-1.1102230246251568e-16*x)-5.0e-324 
+next_awayfrom_zero(x::Float64)   = (x+1.1102230246251568e-16*x)+5.0e-324
 
 # exact for |x| > 4.814825f-35, otherwise may be two steps rather than one step
-next_nearerto_zero(x::Float32)   = muladd(0.99999994f0, x, -1.435f-42)       # (x-5.960465f-8*x)-1.435f-42
-next_awayfrom_zero(x::Float32)   = muladd(1.00000010f0, x, +1.435f-42)       # (x+5.960465f-8*x)+1.435f-42
+next_nearerto_zero(x::Float32)   = (x-5.960465f-8*x)-1.435f-42
+next_awayfrom_zero(x::Float32)   = (x+5.960465f-8*x)+1.435f-42
 
 # the multiplicative formulation for Float16 is exact for |x| > Float16(0.25)
 # which is quite coarse, we do not use that here. Exact for all finite Float16s.
