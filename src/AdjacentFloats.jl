2f436ccb2786a6359022b476cf1d122d89bcf252
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-17 17:11:31 -0400

    speed up by inlining subcalls

--- src/AdjacentFloats.jl
+++ src/AdjacentFloats.jl
@@ -10,17 +10,17 @@ prev_float{T<:IEEE754}(x::T) = signbit(x) ? next_awayfrom_zero(x) : next_nearert
 
 
 # exact for |x| > 8.900295434028806e-308, otherwise may be two steps rather than one step. |x| > ldexp(0.5, -1019)
-next_nearerto_zero(x::Float64)   = (x-1.1102230246251568e-16*x)-5.0e-324 
-next_awayfrom_zero(x::Float64)   = (x+1.1102230246251568e-16*x)+5.0e-324
+@inline next_nearerto_zero(x::Float64)   = (x-1.1102230246251568e-16*x)-5.0e-324 
+@inline next_awayfrom_zero(x::Float64)   = (x+1.1102230246251568e-16*x)+5.0e-324
 
 # exact for |x| > 4.814825f-35, otherwise may be two steps rather than one step
-next_nearerto_zero(x::Float32)   = (x-5.960465f-8*x)-1.435f-42
-next_awayfrom_zero(x::Float32)   = (x+5.960465f-8*x)+1.435f-42
+@inline next_nearerto_zero(x::Float32)   = (x-5.960465f-8*x)-1.435f-42
+@inline next_awayfrom_zero(x::Float32)   = (x+5.960465f-8*x)+1.435f-42
 
 # the multiplicative formulation for Float16 is exact for |x| > Float16(0.25)
 # which is quite coarse, we do not use that here. Exact for all finite Float16s.
-next_nearerto_zero(x::Float16) = signbit(x) ? nextfloat(x) : prevfloat(x)
-next_awayfrom_zero(x::Float16) = signbit(x) ? prevfloat(x) : nextfloat(x)
+@inline next_nearerto_zero(x::Float16) = signbit(x) ? nextfloat(x) : prevfloat(x)
+@inline next_awayfrom_zero(x::Float16) = signbit(x) ? prevfloat(x) : nextfloat(x)
 
 
 end # module
