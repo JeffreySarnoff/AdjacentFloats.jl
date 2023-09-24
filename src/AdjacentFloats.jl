b340073c287fe92be5edce4a5be0f704d6a62653
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-09-24 09:39:45 -0400

    Update AdjacentFloats.jl

--- src/AdjacentFloats.jl
+++ src/AdjacentFloats.jl
@@ -2,9 +2,51 @@ module AdjacentFloats
 
 export prev_float, next_float
 
-import Base: nextfloat, prevfloat
+const halfeps(::Type{Float64}) = eps(Float64)/2
+const halfeps(::Type{Float32}) = eps(Float32)/2
+const halfeps(::Type{Float16}) = eps(Float16)/2
 
+const leastfloat(::Type{Float64}) = nextfloat(zero(Float64))
+const leastfloat(::Type{Float32}) = nextfloat(zero(Float32))
+const leastfloat(::Type{Float16}) = nextfloat(zero(Float16))
 
+next_nearerto_zero(x::T) where {T} = -fma(halfeps(T), x, -x) + leastfloat(T)
+next_awayfrom_zero(x::T) where {T} =  fma(halfeps(T), x,  x) - leastfloat(T)
+
+@inline function prev_float_signed(x::T) where {T}
+    signbit(x) ? next_awayfrom_zero(x) : next_nearerto_zero(x)
+end
+
+@inline function next_float_signed(x::T) where {T}
+    signbit(x) ? next_nearerto_zero(x) : next_awayfrom_zero(x)
+end
+
+@inline function next_float(x::T) where {T}
+    if !isinf(x)
+        next_float_signed(x)
+    else
+        if !signbit(x)
+            x
+        else
+            -floatmax(T)
+        end
+    end
+end
+
+@inline function prev_float(x::T) where {T}
+    if !isinf(x)
+        prev_float_signed(x)
+    else
+        if signbit(x)
+            x
+        else
+            floatmax(T)
+        end
+    end
+end
+
+    
+#=
 function next_float(x::Float64)
     x !== -Inf && return next_float_signed(x) 
     return -realmax(Float64)
@@ -66,9 +108,13 @@ end
     signbit(x) ? next_nearerto_zero(x) : next_awayfrom_zero(x)
 end
 
+@inline next_nearerto_zero(x::Float16) = -fma(+Float16(0.0004883), x, -x) + Float16(6.0e-8)
+@inline next_awayfrom_zero(x::Float16) =  fma(+Float16(0.0004883), x,  x) - Float16(6.0e-8)
+
 @inline next_nearerto_zero(x::Float16) = signbit(x) ? nextfloat(x) : prevfloat(x)
 @inline next_awayfrom_zero(x::Float16) = signbit(x) ? prevfloat(x) : nextfloat(x)
 
+=#
 
 # fallbacks
 
